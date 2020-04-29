0abd5be63d0bbf9fee90499f20763eb611f3c280
ttppss <zgjxxzn@gmail.com>
2020-04-29 00:37:34 -0400

    print out result

--- test.py
+++ test.py
@@ -26,7 +26,7 @@ def eval(dataloader, model, test_num):
 				#print("gt_labesl shape: ", gt_labels_)
 				sizes = [sizes[0][0].item(), sizes[1][0].item()]
 				pred_bboxes_, pred_labels_, pred_scores_ = model.predict(imgs, [sizes])
-				print("pred_bboxes_shape: ", pred_bboxes_.shape, "\n", "pred_bboxes: ", pred_bboxes_, '\n')
+				print("pred_bboxes_shape: ", len(pred_bboxes_), "\n", "pred_bboxes: ", pred_bboxes_, '\n')
 				gt_bboxes += list(gt_bboxes_.numpy())
 				gt_labels += list(gt_labels_.numpy())
 				gt_difficults += list(gt_difficults_.numpy())
