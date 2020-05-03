0d542738d795f247fcd34e720f8149c9f340323c
ttppss <zgjxxzn@gmail.com>
2020-05-03 15:50:22 -0400

    removed RGB2BGR in preprocessing since it was read in as BGR; converted back to RGB during plot.

--- demo_test.py
+++ demo_test.py
@@ -2,4 +2,4 @@ import cv2
 
 img_path = '/data2/dechunwang/dataset/large_dataset/Original/49.png'
 pic = cv2.imread(img_path)
-new_pic = cv2.imwrite('new_49.jpg', pic)
+new_pic = cv2.imwrite('/data1/zinan_xiong/simple-faster-rcnn-pytorch/new_49.jpg', pic)
